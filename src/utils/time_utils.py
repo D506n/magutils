493b25254c8e4f39189214ad82da592c06fd9e5e
D506n <D506n@yandex.ru>
2025-11-08 04:19:42 +0300

    Добавил новые функции для работы со временем

--- src/utils/time_utils.py
+++ src/utils/time_utils.py
@@ -17,3 +17,26 @@ def parse_time(time_str: str):
 @lru_cache()
 def format_time(time_obj: datetime):
     return time_obj.strftime(os.getenv('TIME_FORMAT', '%Y-%m-%dT%H:%M:%S.%f%z'))
+
+def get_delta(dt:datetime, dt2:datetime=None):
+    if dt2 is None:
+        dt2 = get_current_time()
+    return dt2 - dt
+
+def seconds_stringify(seconds: int) -> str:
+    """Преобразует количество секунд в часы/минуты.
+    
+    Args:
+        seconds: Количество секунд для преобразования.
+        
+    Returns:
+        Строка в формате x h./x m./x h. y m..
+    """
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    result = ''
+    if hours>0:
+        result += f'{hours} h. '
+    if minutes>0:
+        result += f'{minutes} m.'
+    return result.strip()
