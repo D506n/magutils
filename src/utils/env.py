4f4e84b7fe8de8aeb9a21a7c4ac8b1fcf4801c7c
D506n <130845734+D506n@users.noreply.github.com>
2026-01-26 00:39:51 +0300

    Добавил строгую валидацию на основе pydantic вместо самописных валидаторов

--- src/utils/env.py
+++ src/utils/env.py
@@ -1,36 +1,103 @@
 import os
-from dataclasses import MISSING, Field, dataclass
 from functools import wraps
 from pathlib import Path
 from typing import get_origin
 
 import orjson as orj
 from dotenv import load_dotenv
+from pydantic import BaseModel, TypeAdapter, ValidationError
+from pydantic.dataclasses import dataclass
+from pydantic_core import PydanticUndefined
 
 
-class __DataclassMock:
-    __dataclass_fields__: dict[str, Field] = {}
-    _prefix: str
+class _PrefixStorage:
+    _storage: dict[type, str] = {}
+
+
+class EnvParsingError(Exception):
+    def __init__(self, cls: type, field: str, expected: type, val):
+        self.prefix = _PrefixStorage._storage[cls]
+        self.field = field
+        self.expected = expected
+        self.val = val
+
+    def __str__(self):
+        return (
+            f'Field {self.prefix}{self.field} parsing error!'
+            'A valid json string representing'
+            f' <{self.expected.__name__}> is expected, '
+            f'received <{type(self.val).__name__}>: {self.val}')
+
+
+class FieldRequiredError(Exception):
+    def __init__(self, cls: type, name: str):
+        self.field_name = name
+        self.prefix = _PrefixStorage._storage[cls]
+
+    def __str__(self):
+        return f'Field {self.prefix}{self.field_name} is required!'
+
+
+class EnvValidationError(Exception):
+    def __init__(self, *errs: FieldRequiredError | EnvParsingError):
+        self.text = 'Environ can\'t be parsed. Errors:\n' + '\n'.join(
+                [str(e) for e in errs])
+
+    def __str__(self):
+        return self.text
+
+
+def _find_env():
+    path = Path(__file__).parent / '.env'
+    if path.exists():
+        return path
+    stop = Path.cwd()
+    while path != stop:
+        path = path.parent
+        if (path / '.env').exists():
+            return path / '.env'
 
 
 def _build_fields(
-        cls: __DataclassMock, 
-        default_priority: bool = False, 
+        cls: type, 
         prefix: str = ''):
-    result = {}
-    for field in cls.__dataclass_fields__.values():
-        if field.default is not MISSING and (
-                not os.getenv(prefix + field.name) or default_priority):
-            result[field.name] = field.default
-        elif field.default_factory is not MISSING and (
-                not os.getenv(prefix + field.name) or default_priority):
-            result[field.name] = field.default_factory()
+    def json_valid(obj: type):
+        orig = get_origin(obj) or obj
+        return orig in {dict, list}
+    pcls = dataclass()(cls)
+    _PrefixStorage._storage[pcls] = prefix
+    result: dict = {}
+    errors: list[ValidationError] = []
+
+    for name, f in pcls.__pydantic_fields__.items():
+        default = f.default if f.default != PydanticUndefined else None
+        dfactory = f.default_factory if f.default_factory else lambda: None
+        env_val = os.getenv(prefix + name) \
+            or default \
+            or dfactory()
+
+        if env_val is None:
+            errors.append(FieldRequiredError(pcls, name))
+            continue
+
+        adapt = TypeAdapter(f.annotation)
+
+        if json_valid(f.annotation):
+            try:
+                data = adapt.validate_json(env_val)
+            except ValidationError:
+                errors.append(
+                    EnvParsingError(pcls, name, f.annotation, env_val))
         else:
-            orig = get_origin(field.type)
-            if orig in {dict, list} or issubclass(field.type, (dict, list)):
-                result[field.name] = orj.loads(os.getenv(prefix + field.name))
-            else:
-                result[field.name] = field.type(os.getenv(prefix + field.name))
+            try:
+                data = adapt.validate_strings(env_val)
+            except ValidationError:
+                errors.append(
+                    EnvParsingError(pcls, name, f.annotation, env_val))
+        result[name] = data
+
+    if errors:
+        raise EnvValidationError(*errors)
     return result
 
 
@@ -55,14 +122,22 @@ class EnvironTools:
         load_dotenv(path, override=True)
         self.__dict__.update(_build_fields(self.__class__, default_priority))
 
-    def save(self, path: Path = None):
+    def save(self, path: Path = None, full_env: bool = False):
         if not path:
-            path = Path.cwd() / '.env'
+            path = _find_env()
+
         with open(path, 'r', encoding='utf-8') as f:
             current_data = {k: v for k, v in 
                             [row.split('=', 1) for row in f.read().split('\n')]}
         new_data = {}
-        for key, value in self.__dict__.items():
+        prefix = _PrefixStorage._storage[self.__class__]
+
+        if full_env:
+            variables = os.environ.items()
+        else:
+            variables = self.__dict__.items()
+
+        for key, value in variables:
             if not callable(value) and not key.startswith('_'):
                 if isinstance(value, (dict, list)):
                     value = orj.dumps(value).decode()
@@ -70,31 +145,35 @@ class EnvironTools:
                     value = str(value)
                 if value == 'True' or value == 'False':
                     value = value.lower()
-                new_data[f'{self._prefix}{key}'] = value
+                if key in self.__dict__.keys():
+                    key = prefix + key
+                new_data[f'{key}'] = value
+
         current_data.update(new_data)
         prepared_data = [f'{k}={v}' for k, v in current_data.items()]
+
         with open(path, 'w') as f:
             f.write('\n'.join(prepared_data))
 
 
 def environ(
         path: Path = None, 
-        default_priority: bool = False, 
         prefix: str = ''):
+
     if not path:
-        path = Path.cwd() / '.env'
+        path = _find_env()
 
     def environ_wrap(cls):
-        instances: dict[type, __DataclassMock] = {}
-        cls = dataclass()(cls)
+        instances: dict = {}
 
         @wraps(cls)
         def wrapper():
             if cls not in instances:
                 load_dotenv(path)
-                fields = _build_fields(cls, default_priority, prefix)
-                instances[cls] = cls(**fields)
-                instances[cls]._prefix = prefix
-            return instances[cls]
+                fields = _build_fields(cls, prefix)
+                pcls = dataclass()(cls)
+                instances[pcls] = pcls(**fields)
+                _PrefixStorage._storage[pcls] = prefix
+            return instances[pcls]
         return wrapper
     return environ_wrap
