56a0b6b184704464eca16053145bd0e5fe0f17ca
d506n <d506n@yandex.ru>
2026-03-05 16:32:30 +0300

    Добавил корректную обработку завершения работы

--- src/utils/logging/handlers/basic.py
+++ src/utils/logging/handlers/basic.py
@@ -2,6 +2,7 @@ import asyncio
 import atexit
 from asyncio import Queue, create_task
 from logging import Handler, LogRecord
+from threading import Event
 
 
 class BaseAsyncHandler(Handler):
@@ -11,6 +12,7 @@ class BaseAsyncHandler(Handler):
         self.queue: Queue[LogRecord] = Queue()
         self.bg_task = None
         self._shutdown_marker = object()
+        self.closing_event = Event()
         atexit.register(self.close)
 
     def emit(self, record):
@@ -26,16 +28,27 @@ class BaseAsyncHandler(Handler):
             self.bg_task = create_task(self.read_queue())
 
     async def read_queue(self, at_exit=False):
-        while True:
+        while not self.closing_event.is_set():
             record = await self.queue.get()
+            if record is self._shutdown_marker:
+                self.queue.task_done()
+                self.queue.shutdown()
+                break
             try:
                 await self.ahandle(record, at_exit=at_exit)
                 self.queue.task_done()
-            except Exception:  # nocov
+            except Exception:
                 self.handleError(record)
 
     async def ahandle(self, record: LogRecord, at_exit=False):
         raise NotImplementedError()
+
+    async def ajoin(self):
+        if self.bg_task\
+        and (self.bg_task.done() or self.bg_task.cancelled())\
+        and not self.queue.empty():
+            await self.read_queue(at_exit=True)
+            self.bg_task = None
 
     def chandle(self, record):
         pass
@@ -44,6 +57,9 @@ class BaseAsyncHandler(Handler):
         pass
 
     def close(self):
+        if self.closing_event.is_set():
+            return
+        self.closing_event.set()
         super().close()
 
         while True:
